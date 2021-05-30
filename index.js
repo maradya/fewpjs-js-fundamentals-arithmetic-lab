39b22c39ec8dda5eb0117d5ee2346a294c651018
Marady <maradyalexis@gmail.com>
2021-05-30 18:43:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31; 
+const multiply = num1 * num2; 
+
+
+const random= 3;
+
+let num3 = 2; 
+let num4 = 2; 
+ const mod = num3 + num4; 
+
+let max = 20;
+Math.max(10, 15, 20); 
